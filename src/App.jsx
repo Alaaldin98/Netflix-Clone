78b82e20b67b745fe085e9c74926105002e8e971
Alaaldin98 <alaaalhanene@gmail.com>
2022-03-05 23:49:13 +0300

    Did Task18

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div>
       <Home/>
-      <Button>Login</Button>
+      {/* <Button>Login</Button> */}
       {/* <p>Hello Pizza  World </p> */}
     </div>
   )
